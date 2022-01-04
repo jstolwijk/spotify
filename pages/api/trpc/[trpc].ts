cddef8b66f71e83dc5eacee76de676aec375cf38
Jesse Stolwijk <stolwijkjesse@gmail.com>
2022-01-04 21:22:49 +0100

    Use refresh token to geenrate token

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,12 +1,43 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
+const getToken = async () => {
+  const res = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`),
+    },
+    body: `grant_type=refresh_token&refresh_token=${process.env.REFRESH_TOKEN}`,
+  });
+
+  return (await res.json()).access_token;
+};
+
+const fetchCurrentlyPlayingTrack = async () => {
+  const response = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+    headers: {
+      Authorization: `Bearer ${await getToken()}`,
+    },
+  });
+
+  return await response.json();
+};
+
+interface GetCurrentActivity {
+  title: string;
+  artists: string[];
+}
+
 const appRouter = trpc.router().query("get-current-activity", {
-  resolve() {
-    return {
-      title: "FORCE OF HABIT",
-      artist: "Paris texas",
+  async resolve() {
+    const body = await fetchCurrentlyPlayingTrack();
+    const res: GetCurrentActivity = {
+      title: body.item.name,
+      artists: body.item.artists.map((artist: any) => artist.name),
     };
+
+    return res;
   },
 });
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,13 @@ import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const activity = trpc.useQuery(["get-current-activity"]);
+  const activity = trpc.useQuery(["get-current-activity"], { refetchInterval: 10000 });
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="flex flex-col border p-4">
         <h1 className="text-3xl font-bold">{activity.data?.title}</h1>
-        <h2 className="text-xl">{activity.data?.artist}</h2>
+        <h2 className="text-xl">{activity.data?.artists?.join(", ")}</h2>
       </div>
     </div>
   );
