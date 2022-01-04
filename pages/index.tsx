613804cb4434f847307c6e1e3c9f622f20bc4cf8
Jesse Stolwijk <stolwijkjesse@gmail.com>
2022-01-04 21:37:13 +0100

    Set default text

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="flex flex-col border p-4">
-        <h1 className="text-3xl font-bold">{activity.data?.title}</h1>
+        <h1 className="text-3xl font-bold">{activity.data?.title || "No music playing 😭"}</h1>
         <h2 className="text-xl">{activity.data?.artists?.join(", ")}</h2>
       </div>
     </div>
